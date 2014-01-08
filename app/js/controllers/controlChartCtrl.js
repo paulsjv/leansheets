4922fd7e0cdd73b06047dbe62482c53bf550ed8c
U-TANT-A01\paujo02 <paujo02@PAUJO02.ca.com>
2014-01-08 13:03:44 -0600

    using the deferred and promise api's to get the data sheet from google's query api.

--- app/js/controllers/controlChartCtrl.js
+++ app/js/controllers/controlChartCtrl.js
@@ -2,6 +2,14 @@
 
 angular.module('controlChartCtrl', []).
   controller('ControlChartCtrl', ['$scope', 'GoogleService', function($scope, GoogleService) {
+  	var data = GoogleService.getData('Feature', 'feature');
   	$scope.helloworld = "hello world!";
+  	data.then(function (success) {
+  		$scope.sheetdata = success;
+  	}, function (error) {
+  		$scope.sheetdata = error;
+  	});
   	console.log(GoogleService.getUrl());
+  	console.log(GoogleService.getData('Feature', 'feature'));
+  	console.log($scope.sheetdata);
   }]);

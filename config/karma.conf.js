43213cd04c463d3c66698230756b89cd1b988c53
Jay <paulsjv@gmail.com>
2014-01-10 07:57:05 -0600

    removing google-sheets.js from testing and renaming jquery directory

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -13,7 +13,8 @@ module.exports = function(config){
     exclude : [
       'app/lib/angular/angular-loader.js',
       'app/lib/angular/*.min.js',
-      'app/lib/angular/angular-scenario.js'
+      'app/lib/angular/angular-scenario.js',
+      'app/js/google-sheets.js'
     ],
 
     autoWatch : true,

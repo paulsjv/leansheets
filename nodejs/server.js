fd1f37c726c4e3c61dc7dec417b3df4f7e7c7c17
Jay <paulsjv@gmail.com>
2014-01-10 12:46:59 -0600

    adding instructions to the server.js file to untrack file so that it does not get overwritten

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,4 +1,11 @@
+/************************************
+This will tell git you want to start ignoring the changes to the file
+git update-index --assume-unchanged path/to/file
+
+When you want to start keeping track again
+git update-index --no-assume-unchanged path/to/file
+************************************/
 var connect = require('connect');
 connect.createServer(
-    connect.static("/git/leansheets/app")
+    connect.static("/path/to/leansheets/app")
 ).listen(8080);

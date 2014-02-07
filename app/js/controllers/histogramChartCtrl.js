17bd9262b6bb7620a2a67fa0bace1a448e9fbc9d
Jay <paulsjv@gmail.com>
2014-02-07 15:30:00 -0600

    adding labels to histograms and removing console statements

--- app/js/controllers/histogramChartCtrl.js
+++ app/js/controllers/histogramChartCtrl.js
@@ -1,116 +1,124 @@
 'use strict';
 
 angular.module('histogramChartCtrl', []).
-  controller('HistogramChartCtrl', ['$scope', 'DataService', '$window', function($scope, DataService, $window) {
-  	var directivePromise = DataService.getData('Feature');
-  	directivePromise.then(function (success){
-  		$scope.featureConfig = $scope.getOptionsForChart('Feature', $scope.parseData(success));
-  	}, function (error) {
-  		alert(error);
-  	});
+    controller('HistogramChartCtrl', ['$scope', 'DataService', '$window', function($scope, DataService, $window) {
+        var directivePromise = DataService.getData('Feature');
+        directivePromise.then(function (success){
+  		      $scope.featureConfig = $scope.getOptionsForChart('Feature', $scope.parseData(success));
+        }, function (error) {
+            alert(error);
+        });
 
-  	var defectsPromise = DataService.getData('Defect');
-  	defectsPromise.then(function (success) {
-  		$scope.defectConfig = $scope.getOptionsForChart('Defect', $scope.parseData(success));
-  	}, function (error) {
-  		alert(error);
-  	});
+        var defectsPromise = DataService.getData('Defect');
+        defectsPromise.then(function (success) {
+            $scope.defectConfig = $scope.getOptionsForChart('Defect', $scope.parseData(success));
+        }, function (error) {
+            alert(error);
+        });
 
-    var defectsPromise = DataService.getData('Intangable');
-    defectsPromise.then(function (success) {
-      $scope.intangableConfig = $scope.getOptionsForChart('Intangable', $scope.parseData(success));
-    }, function (error) {
-      alert(error);
-    });
+        var defectsPromise = DataService.getData('Intangable');
+        defectsPromise.then(function (success) {
+            $scope.intangableConfig = $scope.getOptionsForChart('Intangable', $scope.parseData(success));
+        }, function (error) {
+            alert(error);
+        });
 
-	$scope.getOptionsForChart = function (title, data) {
-		return {
-			options: {
-				chart: {
-					zoomType: 'xy'
-				},
-				xAxis: {
-          	title: {
-            	text: 'Lead Times'
-          	},
-        	categories: data.categories
-        },
-        yAxis: [{
-          min: 0,
-          title: {
-            text: 'Fequency'
-          }
-        },{
-          gridLineWidth: 0,
-          title: {
-            text: 'Percentage of Total'
-          }
-        }],
-        exporting: {
-              sourceWidth: 1600,
-              sourceHeight: 1200
-            
-        }
-			},
-			series: [{
-        data: data.frequency,
-        yAxis: 1,
-        type: 'column'
-			},{
-        data: data.percentOfTotal,
-        type: 'spline'
-      }],
-			title: {
-				text: title + ' Histogram'
-			}
-		}
-	};
-
-$scope.parseData = function (csv) {
-    // key of data array is the lead time
-    // value of data array is the number of times lead time shows in data set 
-    var data = new Array();
-    var obj = {
-      categories: new Array(),
-      frequency: new Array(),
-      percentOfTotal: new Array()
+    $scope.getOptionsForChart = function (title, data) {
+		    return {
+            options: {
+				        chart: {
+                    zoomType: 'xy'
+                },
+                xAxis: {
+                  	title: {
+                      	text: 'Lead Times'
+                  	},
+                  	categories: data.categories
+                },
+                yAxis: [{
+                    min: 0,
+                    title: {
+                        text: 'Percentage of Total'
+                    },
+                    labels: {
+                        formatter: function() {
+                            return this.value + '%'
+                        }
+                    },
+                    opposite: true
+                },{
+                    gridLineWidth: 0,
+                    title: {
+                        text: 'Fequency'
+                    },
+                    opposite: false
+                }],
+                exporting: {
+                    sourceWidth: 1600,
+                    sourceHeight: 1200
+                }
+      			},
+      			series: [{
+                name: 'Frequency',
+                data: data.frequency,
+                yAxis: 1,
+                type: 'column'
+      			},{
+                name: 'Percetange of Total',
+                data: data.percentOfTotal,
+                type: 'spline',
+                tooltip: {
+                    valueSuffix: '%'
+                }
+            }],
+      			title: {
+				        text: title + ' Histogram'
+			      }
+    	}
     };
 
-    var lines = csv.split("\n");
-    lines = $scope.popLastIndexOfArrayIfEmpty(lines);   
-    console.log(csv);
-    console.log(lines);
-    var total = 0;
+    $scope.parseData = function (csv) {
+        // key of data array is the lead time
+        // value of data array is the number of times lead time shows in data set 
+        var data = new Array();
+        var obj = {
+            categories: new Array(),
+            frequency: new Array(),
+            percentOfTotal: new Array()
+        };
 
-    for (var i in lines) {
-      var line = lines[i].split(",");
-      if (!data[line[1]]) {
-        data[line[1]] = 1;
-      } else {
-          data[line[1]]++;
-      }
-      total = total +1;
-    }
-    console.log(total);
-    console.log(data);
-    var cumTotal = 0;
-    $.each(data, function(key, value) {
-      if (value !== undefined && value !== null && value != "") {
-        obj.categories.push(key);
-        obj.frequency.push(value);
-        cumTotal = cumTotal + value;
-        obj.percentOfTotal.push($window.Math.round((cumTotal/total) * 100));
-      }
-    });
-    console.log(obj);
-    return obj;
-  };
+        var lines = csv.split("\n");
+        lines = $scope.popLastIndexOfArrayIfEmpty(lines);   
+        var total = 0;
 
-	$scope.popLastIndexOfArrayIfEmpty = function (arry) {
-      	if (arry[arry.length] == "" || arry[arry.length] == null) {
-        	arry.pop();
-	  	}
-  		return arry;
-	};
+        for (var i in lines) {
+            var line = lines[i].split(",");
+            if (!data[line[1]]) {
+                data[line[1]] = 1;
+            } else {
+                data[line[1]]++;
+            }
+            total = total +1;
+        }
+
+        var cumTotal = 0;
+        $.each(data, function(key, value) {
+            if (value !== undefined && value !== null && value != "") {
+                obj.categories.push(key);
+                obj.frequency.push(value);
+                cumTotal = cumTotal + value;
+                obj.percentOfTotal.push($window.Math.round((cumTotal/total) * 100));
+            }
+        });
+
+        return obj;
+    };
+
+  	$scope.popLastIndexOfArrayIfEmpty = function (arry) {
+        	if (arry[arry.length] == "" || arry[arry.length] == null) {
+          	arry.pop();
+  	  	}
+    		return arry;
+  	};
 
 }]);

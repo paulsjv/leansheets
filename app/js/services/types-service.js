dcb6999209f88aee95faaa885c8bddcbc16714a0
Jay Paulson <paulsjv@gmail.com>
2015-01-06 17:15:29 -0600

    changing class of service types

--- app/js/services/types-service.js
+++ app/js/services/types-service.js
@@ -1,5 +1,5 @@
 angular.module('typesService',[]).service('TypesService',[function() {
 	this.getWorkTypes = function() {
-		return ["Standard","Intangible","Discovery","Defect"];
+		return ["Frontend","Backend","Discovery","Defect"];
 	};
 }]);

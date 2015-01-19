e295786846e5bed071724eff4bc052de5d5380c7
Jay Paulson <paulsjv@gmail.com>
2015-01-19 15:24:34 -0600

    removing the defect type of work

--- app/js/services/types-service.js
+++ app/js/services/types-service.js
@@ -1,5 +1,5 @@
 angular.module('typesService',[]).service('TypesService',[function() {
 	this.getWorkTypes = function() {
-		return ["Frontend","Backend","Discovery","Defect"];
+		return ["Frontend","Backend","Discovery"];
 	};
 }]);

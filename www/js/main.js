6ce030f0675b34501201ef9b95ff441a16b8b431
Jay Paulson <paulsjv@gmail.com>
2015-10-26 18:12:11 -0500

    adding google jsapi loading into app

--- www/js/main.js
+++ www/js/main.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 
 import angular from 'angular';
 import helloWorldDirective from './directives/helloWorldDirective';
-import jsapi from 'https://www.google.com/jsapi';
+import google from './async/google';
 
 /**
  * Testing
@@ -10,3 +10,8 @@ import jsapi from 'https://www.google.com/jsapi';
 export default angular.module('main', [])
     .directive('helloWorld', helloWorldDirective);
 
+// This corresponds with the promise you resolved in www/js/async/google.js
+google.then(($google) => {
+    console.log('Bootstraping Application');
+    console.log($google);
+});

d8404bc9db4f01378f17dbceb8597861e3dd294e
Awadhoot Lele <awadhoot.lele@synerzip.com>
2015-11-30 17:10:48 +0530

    Message to users when there is no data returned in query

--- src/js/modules/leansheets/controllers/ls-applicationController.js
+++ src/js/modules/leansheets/controllers/ls-applicationController.js
@@ -24,6 +24,12 @@ define(['angular'], function (ng) {
 
             $scope.workType;
             $scope.workTypes;
+            $scope.dataStatus = {
+                histogram: false,
+                control: false,
+                cfd: false
+            };
+
 
             $scope.sheetsKeys = Object.keys(configService.getSheets());
             $scope.sheet = $scope.sheetsKeys[0];
@@ -59,11 +65,19 @@ define(['angular'], function (ng) {
                         areDatesValid(obj.startDate, obj.endDate)) {
                     chart.getChart(obj).then(
                         function(success) {
+                            // hide the error message message for the chart
+                            $scope.dataStatus[chartName] = true;
+
                             $log.debug('Firing "chart:' + chartName + '" event: ls-applicationController!');
                             $scope.$broadcast('chart:' + chartName, success);
                         }, function(error) {
+                            $scope.errorMessage = error.message;
+
+                            // show error message for the chart
+                            $scope.dataStatus[chartName] = false;
+
                             $log.debug('Error getting data from Google Sheets!', error);
-                             alert('Error getting data from Google Sheets! ' + error);
+                            alert('Error getting data from Google Sheets! ' + error);
                         });
                 }
             };
@@ -86,7 +100,7 @@ define(['angular'], function (ng) {
                 return dropdowns;
             };
 
-            // returns current date minus number of days to subtract 
+            // returns current date minus number of days to subtract
             // from current date that is supplied in the config.json
             $scope.getDefaultStartDate = function() {
                 return $moment()
@@ -100,7 +114,7 @@ define(['angular'], function (ng) {
             };
 
             var areDatesValid = function(startDate, endDate) {
-                if (!ng.isDefined(startDate) || 
+                if (!ng.isDefined(startDate) ||
                     !ng.isDefined(endDate) ||
                     !isDateValid(startDate) ||
                     !isDateValid(endDate)) {

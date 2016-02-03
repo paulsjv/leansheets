e372f814ede863d01ba87807fb77a438becda782
Jay Paulson <paulsjv@gmail.com>
2016-02-03 16:53:48 -0600

    returning promise with transformed data from dataservice.getdata call

--- www/js/services/dataService.js
+++ www/js/services/dataService.js
@@ -23,16 +23,20 @@ export default class DataService {
         this.log.debug('dataService.js - in getData()'); 
         let extractService = this.dataFactory.getExtractService("Sheet: Demo Team");
         let transformService = this.dataFactory.getTransformService("Sheet: Demo Team");
-        extractService.getData().then(
+        let promise = new Promise((resolve, reject) => {
+         extractService.getData().then(
             (success) => {
                 this.log.debug('dataService.js - success getting data');
                 this.log.debug(success);
-                this.log.debug('dataService.js - transformed data: ', transformService.transformData(success));
+                let transformed = transformService.transformData(success);
+                this.log.debug('dataService.js - transformed data: ', transformed);
+                resolve(transformed);
             }).catch((error) => {
                 this.log.error('dataService.js - error getting data');
                 this.log.debug(error);
+                reject(error);
             });
-
-       return {};
+        });
+       return promise;
     }
 }

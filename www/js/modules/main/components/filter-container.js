ce2048e6ad2f11711197c7fc921e44c93687b186
Jay Paulson <paulsjv@gmail.com>
2016-05-02 12:31:39 -0500

    have data updating when clicking filter button

--- www/js/modules/main/components/filter-container.js
+++ www/js/modules/main/components/filter-container.js
@@ -2,11 +2,11 @@ function filterController($log) {
     'ngInject';
 
     this.name = 'Filter';
-
+    this.filter = 0;
     this.setFilter = function(filter) {
         $log.debug('setFilter', filter);
-        this.filter = filter;
-        this.onFilterChange({ $event: { filter: filter } });
+        this.filter = this.filter ? this.filter = 0 : this.filter = 1;
+        this.onFilterChange({ $event: { filter: this.filter } });
     }
 }
 
--- www/js/modules/main/directives/histogramChart.js
+++ www/js/modules/main/directives/histogramChart.js
@@ -7,21 +7,6 @@ import { format, precisionFixed } from 'd3-format';
 import { transition, active } from 'd3-transition';
 
 var log, x, yOverlay, element, svg, margin, bars, overlayLine, xOverlay, xAxis, yAxisRight, barContainerHeight;
-/*var data = [{ frequency: 2, percentage: 13, leadtime: 2 }, 
-            { frequency: 5, percentage: 25, leadtime: 5 }, 
-            { frequency: 13, percentage: 50, leadtime: 7 }, 
-            { frequency: 3, percentage: 63, leadtime: 10 }, 
-            { frequency: 1, percentage: 68, leadtime: 11 }, 
-            { frequency: 4, percentage: 75, leadtime: 15 }, 
-            { frequency: 2, percentage: 82, leadtime: 17 }, 
-            { frequency: 2, percentage: 90, leadtime: 20 }, 
-            { frequency: 1, percentage: 92, leadtime: 21 }, 
-            { frequency: 12, percentage: 93, leadtime: 22 }, 
-            { frequency: 11, percentage: 94, leadtime: 23 }, 
-            { frequency: 10, percentage: 95, leadtime: 24 }, 
-            { frequency: 3, percentage: 98, leadtime: 25 }, 
-            { frequency: 1, percentage: 100,leadtime: 50 }];
-*/
 let resize = function() {
     // get new width of parent node of svg width
     let svgWidth = getSvgWidth(element);
@@ -111,13 +96,7 @@ let tooltipHide = () => {
 };
 
 let histogramController = function() {
-/*    this.setData = function(data) {
-        this.data = data;
-    }
-    this.getDataSet = function() {
-        return this.dataSet;
-    }
-*/
+
 };
 
 export default ($log) => {
@@ -137,11 +116,12 @@ export default ($log) => {
         restrict: 'E',
         link: (scope, elm, attrs, ctrl) => {
             log.debug('histogramChartDirective.js - in link!');
-            let data = ctrl.dataset;//histogramController().getData();
-            log.debug('histogramChartDirective.js - data', attrs);
-            log.debug('elm', elm);
-            log.debug('scope',scope);
-            // element is defined at top of file
+            scope.$watch('histogramController.dataset', function() {
+                log.debug('data set changed!', ctrl.dataset);
+            });
+            let data = ctrl.dataset;
+            log.debug('data', data);
+           // element is defined at top of file
             element = elm[0];
 
             // add the tooltip div for mouse overs

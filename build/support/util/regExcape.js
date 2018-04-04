312b6f82b6f094f893e911e9b9e9d1b10d5a85bb
Jay Paulson <paulsjv@gmail.com>
2018-04-04 15:30:28 -0500

    first commit for beta

--- build/support/util/regExcape.js
+++
@@ -1,4 +0,0 @@
-// regExcape
-// ---------
-// Escapes a string so that it may be used in a RegExp constructor.
-export default (regexStr) => regexStr.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
--- www/js/utils/hasOwnProp.js
+++
@@ -1,5 +0,0 @@
-import has from 'lodash.has';
-
-export default function hasOwnProp(a, b) {
-    return has(a, b);
-}

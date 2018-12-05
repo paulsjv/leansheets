8e18dbfb6374b524b80fa3e1f5c4690095af44c6
Jay Paulson <paulsjv@gmail.com>
2018-12-05 17:01:59 -0600

    implementing layout and design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { DateModule } from './shared/date/date.module';
-import { AppRoutingModule } from './app.routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { WipComponent } from './containers/wip/wip.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WipComponent
+    WipComponent,
+    SignupComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +28,9 @@ import { WipComponent } from './containers/wip/wip.component';
     MaterialModule,
     HttpClientModule,
     DateModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
+import {
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
+} from '@angular/material';
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+	MatButtonModule, 
+	MatIconModule,
+	MatFormFieldModule,
+	MatInputModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule],
+    exports: [
+	MatButtonModule, 
+	MatIconModule,
+	MatFormFieldModule,
+	MatInputModule,
+    	MatSidenavModule,
+        MatToolbarModule,
+        MatListModule]
 })
 export class MaterialModule {}
 
